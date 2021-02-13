db4b3a550e122d1448a6a72faafd99af368f0de6
heyqule <hq.jfax@gmail.com>
2021-02-13 18:06:54 -0500

    1.0.0 commit

---
+++ control.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: heyqule
+-- Date: 02/14/2021
+-- Time: 5:04 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+
+
+
---
+++ data.lua
@@ -0,0 +1,10 @@
+require('__erm_terran__/global')
+
+require "prototypes.entities.battlecuirser"
+require "prototypes.entities.wraith"
+require "prototypes.entities.tank"
+require "prototypes.entities.marine"
+
+require "prototypes.items"
+require "prototypes.recipes"
+require "prototypes.technologies"
