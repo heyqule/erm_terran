0af353391cef86e409543fef996a726922bbecd0
heyqule <hq.jfax@gmail.com>
2021-07-03 14:30:05 -0400

    1.3.0 update

--- prototypes/entities/battlecuirser.lua
+++ prototypes/entities/battlecuirser.lua
@@ -11,6 +11,7 @@ require('__erm_terran__/global')
 local ERM_UnitTint = require('__enemyracemanager__/lib/unit_tint')
 
 local ERM_Config = require('__enemyracemanager__/lib/global_config')
+local ERMDataHelper = require('__enemyracemanager__/lib/helper/data_helper')
 local TerranSound = require('__erm_terran__/prototypes/sound')
 local name = 'battlecruiser'
 
@@ -26,7 +27,6 @@ local unit_scale = 1.5
 
 local collision_box = { { -0.25, -0.25 }, { 0.25, 0.25 } }
 local selection_box = { { -2.0, -2.0 }, { 2.0, 2.0 } }
-
 
 data:extend({
     {
@@ -52,7 +52,7 @@ data:extend({
             { type = "cold", percent = 75 }
         },
         healing_per_tick = 0,
-        collision_mask = { 'layer-33' },
+        collision_mask = ERMDataHelper.getFlyingCollisionMask(),
         collision_box = collision_box,
         selection_box = selection_box,
         sticker_box = selection_box,
