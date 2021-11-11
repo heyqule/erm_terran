e5bc23189ca12e3d6bdfe7a974101a24fa11f53b
heyqule <hq.jfax@gmail.com>
2021-11-10 21:35:01 -0500

    Low MK1 tank HP

--- prototypes/entities/tank.lua
+++ prototypes/entities/tank.lua
@@ -49,7 +49,7 @@ data:extend({
         },
         flags = { "placeable-enemy", "placeable-player", "placeable-off-grid", "player-creation", "not-flammable" },
         has_belt_immunity = true,
-        max_health = 180 * ERMPlayerUnitHelper.get_health_multiplier(),
+        max_health = 150 * ERMPlayerUnitHelper.get_health_multiplier(),
         order = MOD_NAME .. name,
         subgroup = "erm_controlable_units",
         shooting_cursor_size = 2,
