a1ee2d0fdc04a62d8ffb948b682b7e786e6542c1
heyqule <hq.jfax@gmail.com>
2022-11-16 00:13:23 -0500

    Add Army Control GUI

--- control.lua
+++ control.lua
@@ -8,7 +8,7 @@
 require('global')
 local Event = require('__stdlib__/stdlib/event/event')
 local String = require('__stdlib__/stdlib/utils/string')
-local ErmConfig = require('__enemyracemanager__/lib/global_config')
+local gui = require("__enemyracemanager__/gui/army_control_window")
 
 local populations = {
     ['battlecruiser'] = 5,
@@ -21,7 +21,7 @@ local refresh_data = function()
     for _, prototype in pairs(game.get_filtered_entity_prototypes({{filter = "type", type = "unit"}})) do
         local nameToken = String.split(prototype.name, '/')
         if nameToken[1] == MOD_NAME and populations[nameToken[2]] then
-            remote.call('enemy_race_manager','army_register_unit', prototype.name, populations[nameToken[2]]);
+            remote.call('enemy_race_manager','army_units_register', prototype.name, populations[nameToken[2]]);
         end
     end
 end
@@ -32,4 +32,12 @@ end)
 
 Event.on_configuration_changed(function(event)
     refresh_data()
+    for _, player in pairs(game.connected_players) do
+        gui.update_overhead_button(player.index)
+    end
+end)
+
+
+Event.register(defines.events.on_player_created, function(event)
+    gui.update_overhead_button(event.player_index)
 end)
